65c01b75e20314527479e5c6b997c41caa9f4ed3
MIGUEL JOAQUIM DE OLIVEIRA <22282161@CFP127ALUNO.LOCAL>
2023-03-31 11:00:13 -0300

    desenvolvendo

--- exercicio/lion_school/modulos.js
+++ exercicio/lion_school/modulos.js
@@ -13,23 +13,89 @@ function getCursos (){
   cursosJson.cursos.forEach(function(cursosFill){
 
     
-        array.push(cursosFill.nome)
+        array.push(cursosFill.sigla)
     
 
     })
 
-    json = {aaaa: array}
+    json = {cursos: array}
 
     return json
 }
-
-console.log(getCursos())
-
-
+//console.log(getCursos())
 
 function getAlunos(){
 
     let json = {}
 
-}
+    let array = []
 
+    alunosJson.alunos.forEach(function(alunosFillter){
+
+    
+        array.push(alunosFillter.nome)
+    
+
+    })
+
+    json = {cursos: array}
+
+    return json
+
+
+
+}
+//console.log(getAlunos())
+
+function getDados(){
+
+   
+    let dadosJ = {}
+
+   let array = []
+
+   alunosJson.alunos.forEach(function(alunos){
+
+       let json = {foto: alunos.foto,
+           nome : alunos.nome, 
+           matricula: alunos.matricula,
+           sexo:alunos.sexo,
+           cursos: alunos.curso}
+
+       array.push(json)   
+
+   })
+   dadosJ.dados = array
+   return dadosJ
+
+}
+//console.log(getDados())
+
+
+
+function getSpecificDados(matricula){
+
+    let json = {
+    }
+     let dadosJ = {}
+
+    let array = []
+
+    alunosJson.alunos.forEach(function(dadosFillter){
+
+        if(matricula == dadosFillter.matricula){
+
+            json = {foto : dadosFillter.foto, nome: dadosFillter.nome,}
+            array.push(json)
+        }
+
+    })
+    dadosJ.dados = array
+    return dadosJ
+
+}
+//console.log(getSpecificDados('20151001014'))
+
+function getAlunosCursos(){
+    
+}
